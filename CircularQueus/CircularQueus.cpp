82903f22b074514b1fc07a826b041527d1f05509
ihsan putra riadi <iputrariadi@gmail.com>
2023-06-22 11:16:33 +0700

    membuat front position

--- CircularQueus/CircularQueus.cpp
+++ CircularQueus/CircularQueus.cpp
@@ -78,6 +78,15 @@ public:
 				cout << queue_array[FRONT_position] << " ";
 				FRONT_position++;
 			}
+			cout << endl;
+		}
+		else {
+			//jika front_position > rear_position, iterasi dari front hingga akhir array
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
+
 
 		}
 		}
